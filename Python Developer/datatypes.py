983cf76551b20655c1eee3dae654090df8563c64
Young Nyamkondiwa <youngnyamkondiwa@gmail.com>
2023-03-27 18:21:45 +0200

    Datatype int and float commit

---
+++ Python Developer/datatypes.py
@@ -0,0 +1,36 @@
+#Fundamental Data Types
+
+#int
+#float
+
+print(4+2)        # int is the result.
+print(4-2)
+print(2*4)
+print(2/4)        # float is the result.
+print(2 ** 2)     # to the power of
+print(5//2)       # returns an int rounded down no fractions.
+print(5 % 2)      # prints the remainder .
+
+print(type(4-2))  # prints the type of this calculation which is int.
+print(type(2/4))  # prints the type of this calculation which is float.
+
+#Maths functions on numbers
+
+print(round(2.5))  # rounding off the number.
+print(abs(-25))    # prints the absolute value of a number no negative 25
+
+#python3 maths functions google for more functions.
+
+#Fundamental Data Types
+
+#boolean  -bool
+#string -str
+#list
+#tuples
+#dictionary -dict
+#set
+
+
+
+
+
